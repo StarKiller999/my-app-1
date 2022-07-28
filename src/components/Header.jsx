ca5becc1a4ce942afaca65af596c607b2b7e5dcd
Alexandr <ordamaleus@gmail.com>
2022-07-29 01:02:51 +0500

    added new files

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+ 
+      <header className='header'>
+        <img src='https://assets.turbologo.ru/blog/ru/2018/03/18170838/prozrachniy-logo.png'></img>
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const Profile = () => {
+  return (
+
+    <div className='content'>
+        <div>
+          <img src='https://u-stena.ru/upload/iblock/697/69702d59285e3f57fd0e92df595b3d5b.jpg'></img>
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>Post1</div>
+            <div>Post2</div>
+          </div>
+        </div>
+        </div>
+  )
+}
+
+export default Profile;
