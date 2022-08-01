dcdef881ae36a1f900bceda2a5356383b95ed4cb
Alexandr <ordamaleus@gmail.com>
2022-08-01 06:29:58 +0500

    14 lesson

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Header.module.css';
 
 
 const Header = () => {
   return (
  
-      <header className='header'>
+      <header className={s.header}>
         <img src='https://assets.turbologo.ru/blog/ru/2018/03/18170838/prozrachniy-logo.png'></img>
       </header>
   )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 
 const Profile = () => {
   return (
 
-    <div className='content'>
+    <div className={s.content}>
         <div>
           <img src='https://u-stena.ru/upload/iblock/697/69702d59285e3f57fd0e92df595b3d5b.jpg'></img>
         </div>
@@ -16,9 +17,9 @@ const Profile = () => {
           <div>
             New post
           </div>
-          <div>
-            <div>Post1</div>
-            <div>Post2</div>
+          <div className={s.posts}>
+            <div className={s.item}>Post1</div>
+            <div className={s.item}>Post2</div>
           </div>
         </div>
         </div>
